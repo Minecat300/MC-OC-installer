8fc0875b61c706685e761895d42cf767b73a36b2
Minecat300 <120320914+Minecat300@users.noreply.github.com>
2025-07-02 22:52:56 +0200

    clears screen when autoupdating on boot

--- bootrun.lua
+++ bootrun.lua
@@ -1,3 +1,6 @@
 package.path = "/Uinstall/?.lua;" .. package.path
 local packInstaller = require("packageInstaller")
+local term = require("term")
+term.clear()
+term.setCursor(1, 1)
 packInstaller.autoUpdateAll()
