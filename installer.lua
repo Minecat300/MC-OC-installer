0c848ce4b37eff0a41a953ae2c39d468327bbb7d
Minecat300 <120320914+Minecat300@users.noreply.github.com>
2025-07-02 02:35:44 +0200

    test
    t

--- installer.lua
+++ installer.lua
@@ -33,8 +33,8 @@ installFile(repoUrl .. "/projectInstaller.lua", installpath .. "/projectInstalle
 print("installed required files")
 
 os.sleep(0.2)
-print(package.path)
+print("package 1: " .. package.path)
 package.path = installpath .. "/?.lua;" .. package.path
-print(package.path)
+print("package 2: " .. package.path)
 local proInstaller = require("projectInstaller")
 proInstaller.install(repoUrl)
